730f84663f99fc93529d2f93194a1d350989b563
Dinh Thuy Anh <dinhthuyanha@gmail.com>
2026-07-05 15:02:40 +0700

    cap nhat db

--- app/Controller/CartController.php
+++ app/Controller/CartController.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Controller;
 
+use App\Middleware\AuthMiddleware;
+
 class CartController {
     public function index() {
+        AuthMiddleware::requireUser();
         require __DIR__ . '/../Views/cart.php';
     }
 
